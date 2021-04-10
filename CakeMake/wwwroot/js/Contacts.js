66eea5c74eefd67f3451f49e4e1a129dfb8dd77d
Tx-wan <77199620+Tx-wan@users.noreply.github.com>
2021-04-10 15:41:19 +0800

    react work

---
+++ CakeMake/wwwroot/js/Contacts.js
@@ -0,0 +1,14 @@
+﻿class Contacts extends React.Component {
+
+
+
+    render() {
+        return (
+            <div className="Contacts">
+                <p>hi react</p>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(Contacts, document.getElementById('content'));
